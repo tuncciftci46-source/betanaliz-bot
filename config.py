4824f81ce7f43222e13d7ab95f437083ee04cb1a
tuncciftci46-source <tuncciftci46-source@users.noreply.github.com>
2026-05-19 16:02:16 +0300

    Avrupa, ABD ve Güney Amerika ligleri eklendi (29 lig)

--- config.py
+++ config.py
@@ -5,5 +5,12 @@ ESPN_BASE = "https://site.api.espn.com/apis/site/v2/sports/soccer"
 LEAGUES = [
     "eng.1", "esp.1", "ita.1", "ger.1", "fra.1",
     "ned.1", "por.1", "tur.1", "bra.1", "arg.1",
-    "uefa.champions", "uefa.europa",
+    "sco.1", "bel.1", "aut.1", "gre.1",
+    "den.1", "nor.1", "swe.1", "rus.1",
+    "cyp.1", "irl.1",
+    "uefa.champions", "uefa.europa", "uefa.europa.conf",
+    "uefa.nations",
+    "usa.1", "usa.nwsl",
+    "bra.2",
+    "conmebol.libertadores", "conmebol.sudamericana",
 ]
